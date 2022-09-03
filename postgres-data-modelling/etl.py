260df7d302faf2b1c67e3c165a235898f460af6f
shubhwip <shubhrjain7@gmail.com>
2022-09-03 22:57:23 +0530

    Finish Initial ETL Pipeline

--- postgres-data-modelling/etl.py
+++ postgres-data-modelling/etl.py
@@ -7,37 +7,39 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    dataframe = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = dataframe[['song_id', 'title', 'artist_id', 'year', 'duration']].values.tolist()[0]
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = dataframe[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values.tolist()[0]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    df.ts = pd.to_datetime(df['ts'])
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    # time_data = 
+    # column_labels = 
+    time_df = pd.DataFrame().assign(start_time=df['ts'], hour=df['ts'].dt.hour,
+     day=df['ts'].dt.dayofweek, week=df['ts'].dt.isocalendar().week, 
+     month=df['ts'].dt.month, year=df['ts'].dt.year, weekday=df['ts'].dt.dayofweek > 4)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -47,6 +49,9 @@ def process_log_file(cur, filepath):
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
+        song_select = ("""
+            SELECT songs.song_id, songs.artist_id FROM (songs JOIN artists ON songs.artist_id = artists.artist_id) WHERE (title = %s and name = %s and duration = %s);
+        """)
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
@@ -56,8 +61,9 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
+        if row.userId != '':
+            songplay_data = (index+1, row.ts, int(row.userId), row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+            cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
@@ -80,7 +86,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=shubhamjain password=")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
